bfefbeafac775784b88140fd717bbe7141af5128
Hemrish <66361650+Hemrish@users.noreply.github.com>
2021-09-02 14:58:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = parseInt(principal * years * rate /100);
+    var year = new Date().getFullYear()+parseInt(years);
     
+    if(principal== null){
+    		alert("Please enter an amount");
+       	document.getElementById("principal").focus();
+        return false;
+    } else if(principal <= 0){
+    		alert("Enter a positive number");
+       	document.getElementById("principal").focus();
+        return false;
+    
+    }
+    
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
